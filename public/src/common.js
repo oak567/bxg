1ad8b430c605bd66afd5f9beac3820a1aed5685f
oak567 <416888031@qq.com>
2017-04-19 13:19:12 +0800

    完成了侧边导航栏个人资料

--- public/src/common.js
+++ public/src/common.js
@@ -61,6 +61,7 @@ define(['jquery', 'template' ,'cookie'], function ($,template) {
                 <h4><%= tc_name %></h4>';
     var render=template.compile(source);
     var html=render(loginfo);
+    //将拼凑好数据的HTML添加到DOM中
     $('.aside .profile').append(html);
 
     // 退出登录
